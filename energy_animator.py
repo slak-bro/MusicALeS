0378b876304bcb85d1c747d7f017cc2b8401bf4c
Arthur Pastel <arthur.pastel@gmail.com>
2019-02-17 00:01:48 +0100

    Fix nan bug and increased FPS

--- energy_animator.py
+++ energy_animator.py
@@ -3,14 +3,14 @@ import numpy as np
 class EnergyAnimator(Animator):
     def __init__(self, audio_source, screen):
         super().__init__(audio_source, screen)
-        self.audio_source.configure(44100, 44100//10)
+        self.audio_source.configure(44100, 44100//30)
         self.audio_source.register_callback(self.animate)
         self.val = 255
         self.max = 0
     def animate(self, data):
         energy = np.sum(np.square(data))
         self.max = max(energy, self.max)
-        v = int(153 * energy /self.max)
+        v = int(153 * energy /self.max) if self.max > 0 else 0
         self.screen.display([[v,0,v] for i in range(self.screen.nLeds)])
         
 
