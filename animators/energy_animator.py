dcd0977fde5f58129dc1d32944eba5f05f46a482
Yoctoboy <alexis.reymann@telecom-paristech.fr>
2019-02-25 23:05:00 +0100

    added smoothing for basic energy animator

--- animators/energy_animator.py
+++ animators/energy_animator.py
@@ -3,27 +3,37 @@ import numpy as np
 
 def grad(ca,cb,i,n):
     return [int((ca[k]+cb[k])*(i/n)) for k in range(3)]
+
+
 class EnergyAnimator(Animator):
     def __init__(self, audio_source, screen):
         super().__init__(audio_source, screen)
         self.audio_source.configure(44100, 44100//30)
         self.audio_source.register_callback(self.animate)
         self.val = 255
-        self.max = 0
+        self.buffer = [0 for _ in range(20)]
+        self.last_value = 0.
+
+    def smooth_value(self, value):
+        result = max(value, self.last_value - 5)
+        self.last_value = int(result)
+        return int(result)
+
     def animate(self, data):
         energy = np.sum(np.square(data))
-        self.max = max(energy, self.max)
-        v = int(153 * energy /self.max) if self.max > 0 else 0
+        max_val = max(energy, max(self.buffer))
+        self.buffer = self.buffer[1:] + [energy]
+        v = self.smooth_value(int(155 * energy / max_val) if max_val > 0 else 0)
         self.screen.display([grad([v,0,v],[0,0,0],i,self.screen.nLeds) for i in range(self.screen.nLeds)])
         
 
 
 if __name__ == "__main__":
     from audio_sources.file_audio_source import FileAudioSource
-    from audio_sources.alsa_audio_source import ALSAAudioSource    
+    #from audio_sources.alsa_audio_source import ALSAAudioSource    
     from screens.sdl_color_screen import SDLColorScreen
-    #audio_source = FileAudioSource("traindata/cvrl-subterfuge.mp3")
-    audio_source = ALSAAudioSource()
+    audio_source = FileAudioSource("traindata/skeler-in_my_mind.mp3")
+    #audio_source = ALSAAudioSource()
     screen = SDLColorScreen(50)
     animator = EnergyAnimator(audio_source, screen)
     animator.start()
