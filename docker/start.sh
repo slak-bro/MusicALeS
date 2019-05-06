e16312ecbb1b43b05da664a0b5a31fbca5ab5ab9
Arthur Pastel <arthur.pastel@gmail.com>
2019-05-06 06:31:15 +0200

    Improved launch and start scripts

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #Enable capture device
-amixer -c 0 cset numid=18,iface=MIXER,name='Mic1 Capture Switch' on,on
+amixer -c 0 cset numid=18,iface=MIXER,name='Mic1 Capture Switch' on,on > /dev/null && \
+echo "Capture device enabled"
+
 #Start program
 cd /app
-python3 main.py --screen serial --alsa hw:CARD=Codec,DEV=0 -n 300
+python3 main.py $@
